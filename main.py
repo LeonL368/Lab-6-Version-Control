acf1c44ddd4deae7c1024205dbf4bafdb9603c8e
theezanonymous <leonliu509@gmail.com>
2024-10-20 23:01:38 -0400

    Create encode and decode functions inside main method with menu

---
+++ main.py
@@ -0,0 +1,34 @@
+#By Leon Liu
+import sys
+def encode(s):
+    res = ""
+    for char in s:
+        res+= str((int(char)+3)%10)
+    return res
+def decode(s):
+    res = ""
+    for char in s:
+        res+=str((int(char)-3)%10)
+    return res
+def main():
+    while True:
+        print("\n--------\nMenu:\n1.Encode\n2.Decode\n3.Quit\n--------\n")
+        try:
+            choice = int(input("Enter an Option: "))
+            if choice == 3:
+                sys.exit()
+            elif choice ==1:
+                s = input("Enter a string to encode: ")
+                print(f"{s} encoded is {encode(s)}")
+            elif choice ==2:
+                s = input("Enter a string to decode: ")
+                print(f"{s} decoded is {decode(s)}")
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid. Try Again.")
+        
+
+
+if __name__=="__main__":
+    main()
